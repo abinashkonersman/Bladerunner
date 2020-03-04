a5341efe9190eac6e88c2315f40b520b60e90953
abinash biswal Konersman <dougabi@yahoo.com>
2020-03-04 07:55:17 -0600

    Update users.sql

--- users.sql
+++ users.sql
@@ -4,3 +4,11 @@ CREATE TABLE `users` (
   `rfid` varchar(70) NOT NULL,
   `timemodified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `usa` (
+    `sid` bigint(11) NOT NULL PRIMARY KEY,
+    `answer` varchar(50) NOT NULL,
+    `timemodified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `uid` bigint(11),
+    FOREIGN KEY (uid) REFERENCES users(uid))ENGINE=InnoDB DEFAULT CHARSET=utf8;
